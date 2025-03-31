7177bf8c3585dc7ac860c9d8f927878984657582
bumbytooles532 <bumbytooles532@gmail.com>
2025-03-31 12:42:31 +0000

    Add 22.php

---
+++ src/22.php
@@ -0,0 +1,9 @@
+<?php
+// This is an example PHP code to demonstrate how to use some basic mathematical functions and variables
+
+function sum($a, $b) {
+    return $a + $b;
+}
+
+$sumResult = sum(3, 5);
+echo "The result of adding 3 and 5 is: " . $sumResult;

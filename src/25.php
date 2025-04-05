859e663f5deba5c503b45ac93da8805cf5eb2810
bumbytooles532 <bumbytooles532@gmail.com>
2025-04-05 06:11:25 +0000

    Add 25.php

---
+++ src/25.php
@@ -0,0 +1,8 @@
+<?php
+
+// Define variables and constants
+$variable1 = "Variable 1";
+const constant1 = "Constant 1";
+
+// Perform some operations using variables
+echo $variable1 . " is equal to " . constant1;

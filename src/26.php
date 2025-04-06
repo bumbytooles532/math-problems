7e1681aaff57cfe474580098dcfa2929769d21ff
bumbytooles532 <bumbytooles532@gmail.com>
2025-04-06 13:47:25 +0000

    Add 26.php

---
+++ src/26.php
@@ -0,0 +1,12 @@
+<?php
+// Example PHP code to solve a problem related to geometry
+function calculate_rectangle_area($length, $width) {
+    return $length * $width;
+}
+
+$length = 10;
+$width = 5;
+
+$result = calculate_rectangle_area($length, $width);
+echo "The area of the rectangle is: " . $result;
+?>

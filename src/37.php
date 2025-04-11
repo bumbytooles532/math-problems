197257ec1861a9aa71ff61c6f17e9fc4e7ad960d
bumbytooles532 <bumbytooles532@gmail.com>
2025-04-11 12:37:06 +0000

    Add 37.php

---
+++ src/37.php
@@ -0,0 +1,17 @@
+<?php
+// Sample PHP code to demonstrate basic mathematical operations
+$first_number = 5;
+$second_number = 3;
+
+$result = $first_number + $second_number; // Addition
+echo "The sum is: $result";
+
+$result = $first_number - $second_number; // Subtraction
+echo "The difference is: $result";
+
+$result = $first_number * $second_number; // Multiplication
+echo "The product is: $result";
+
+$result = $first_number / $second_number; // Division
+echo "The quotient is: $result";
+?>

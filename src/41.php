a35c27961dafa1c0c09ab0f1b4bc508793557025
bumbytooles532 <bumbytooles532@gmail.com>
2025-04-11 12:45:10 +0000

    Add 41.php

---
+++ src/41.php
@@ -0,0 +1,6 @@
+<?php
+$students = array("Alice", "Bob", "Charlie");
+$miles = 5;
+$total_distance = $students * $miles;
+echo "The total distance covered by all students is: " . $total_distance;
+?>

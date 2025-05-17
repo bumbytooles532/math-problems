aec770770b71669cc3323ff3ab82819c5da330ea
bumbytooles532 <bumbytooles532@gmail.com>
2025-05-17 04:13:29 +0000

    Add 63.php

---
+++ src/63.php
@@ -0,0 +1,7 @@
+<?php
+// Example PHP code to demonstrate basic arithmetic operations
+echo 5 + 3; // Addition
+echo 5 - 3; // Subtraction
+echo 5 * 3; // Multiplication
+echo 5 / 3; // Division
+?>
